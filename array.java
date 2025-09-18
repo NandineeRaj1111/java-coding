22ada5f0f24dc78a1e92fa2311929cffc699e45a
NANDINEE RAJ <technonr2307@gmail.com>
2025-09-18 20:13:19 +0530

    sum of array

--- array.java
+++ array.java
@@ -3,10 +3,18 @@ import java.util.Scanner;
 public class array {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
+        System.out.println("enter size");
         int size= sc.nextInt();
         int[] a= new int[size];
+        System.out.println("enter elements");
         for(int i=0;i<size;i++){
             a[i]=sc.nextInt();
         }
+        System.out.println("the array elements are:");
+        for(int i=0;i<size;i++){
+            System.out.print(a[i]+" ");
+        }
+        sc.close();
     }
 }
+
