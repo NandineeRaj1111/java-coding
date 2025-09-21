4e2e2352ff90b2029438758a233e9dc296001c63
NANDINEE RAJ <technonr2307@gmail.com>
2025-09-21 21:01:48 +0530

    creating obj and class

--- Car.java
+++ Car.java
@@ -3,4 +3,12 @@ public class Car {
     String model="Mustang";
     int year=2025;
     boolean isRunning= false;
+    void start(){
+         isRunning=true;
+        System.out.println("You start the engine");
+    }
+    void stop(){
+        isRunning=false;
+        System.out.println("You stop the engine");
+    }
 }
--- oops.java
+++ oops.java
@@ -7,10 +7,17 @@ public class oops {
         Random random= new Random();
         // importing from Car class and creating an obj named car
         Car car = new Car();
+        Car car1= new Car();  //both the obj have same attributes since both belong to the same class
         // accessing different attributes of the obj 
         System.out.println(car.model);
         System.out.println(car.year);
         System.out.println(car.make);
         System.out.println(car.isRunning);
+        car.start();
+        System.out.println(car.isRunning);
+        car.stop();
+        System.out.println(car.isRunning);
+        System.out.println(car1.model);
+        
     }
 }
