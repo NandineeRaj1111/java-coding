c2989ef0f747004780f8f4f83b1aa7dbdb989cd9
NANDINEE RAJ <technonr2307@gmail.com>
2025-09-21 13:25:08 +0530

    learning methods

---
+++ methods.java
@@ -0,0 +1,10 @@
+public class methods {
+    public void main(String[] args) {
+     HBD();   
+    }
+    void HBD(){
+        System.out.println("HAPPY BIRTHDAY TO YOU");
+        System.out.println("I wish you a very happy birthday dear");
+    }
+}
+
--- string.java
+++ string.java
@@ -4,12 +4,40 @@ public class string {
         s.concat("Raj");
         // concat() cant add Raj because string is immutable
         System.out.println(s);
+        // string methods
+        String name= "Harshit Gaur";
+        int length=name.length();
+        int index=name.indexOf(" ");
+        char letter=name.charAt(0);
+        int lastIndex=name.lastIndexOf("a");
+         name=name.toUpperCase();
+        // name=name.trim();
+        // name=name.replace("i","u");
+        System.out.println(name);
+        if(name.isEmpty()){
+            System.out.println("Name is empty");
+        }
+        else{
+            System.out.println("NICE Name");
+        }
+        if(name.equals(s)){
+            System.out.println("Name matched !!!");
+        }
+        else{
+            System.out.println("Afsos! name didn't match ");
+        }
+        System.out.println(lastIndex);
+        System.out.println(length);
+        System.out.println(letter);
+        System.out.println(index);
+
+        
     }
 }
-class name{
+class name {
     public static void main(String[] args) {
-        String name="Binit";
-        name=name.concat(" Chaudhary");
+        String name="Nandinee";
+        name=name.concat(" Raj");
         System.out.println(name);
     }
 }
