fa1c9adfc060b47bcfc62054c31190539a58436f
NANDINEE RAJ <technonr2307@gmail.com>
2025-09-21 21:01:25 +0530

    learning oops

---
+++ Car.java
@@ -0,0 +1,6 @@
+public class Car {
+    String make="Ford";
+    String model="Mustang";
+    int year=2025;
+    boolean isRunning= false;
+}
--- oops.java
+++ oops.java
@@ -1,5 +1,16 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class oops {
     public static void main(String[] args) {
-        
+        Scanner sc= new Scanner(System.in);
+        Random random= new Random();
+        // importing from Car class and creating an obj named car
+        Car car = new Car();
+        // accessing different attributes of the obj 
+        System.out.println(car.model);
+        System.out.println(car.year);
+        System.out.println(car.make);
+        System.out.println(car.isRunning);
     }
 }
