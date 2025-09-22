0d81348df279690e59fd15fe7d2e8f9ad155b67d
NANDINEE RAJ <technonr2307@gmail.com>
2025-09-22 21:35:37 +0530

    learning overloading constructor

---
+++ User.java
@@ -0,0 +1,15 @@
+public class User {
+    String username;
+    int id;
+    String email;
+    User(String username, String email){
+        this.username= username;
+        this.email= email;
+        
+    }
+    User(int id){
+        this.id = id;
+    }
+    
+
+}
---
+++ main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+        User user1= new User("Nandinee","thegr8nr@gmail.com");
+        User user2= new User("Harsh","harshrajput@gmail.com");
+        User user1 = new User(123456);
+        System.out.println(user1.username);
+        System.out.println(user2.email);
+
+    }
+}
