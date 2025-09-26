6fa4f46a68613cde6158ae5f3b6e3e016cc84daa
NANDINEE RAJ <technonr2307@gmail.com>
2025-09-26 16:15:10 +0530

    learning interfaces

---
+++ interfacing.java
@@ -0,0 +1,13 @@
+public class interfacing {
+    public static void main(String[] args) {
+        rabbit Rabbit= new rabbit();
+        Rabbit.flee();
+        hawk Hawk= new hawk();
+        Hawk.hunt();
+        fish Fish = new fish();
+        Fish.flee();
+        Fish.hunt();
+
+    }
+    
+}
